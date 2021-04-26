240195e1f436e22765d591245d702b05e0a2db40
wire-dotcom <wire03001@utb.vaxjo.se>
2021-04-26 10:37:35 +0200

    Commit 2: Börjat på meny, har en pekare som ska användas för att välja de olika valen i menyn men fungerar inte korrekt ännu. Impleneterat en variable "gamestate" som kan användas för att bestämma när sprites ska ritas upp på skärmen eller inte (till exempel så ska spelaren inte ritas upp innan spelet är igång).

--- VideoGameProject/Game1.cs
+++ VideoGameProject/Game1.cs
@@ -17,6 +17,8 @@ namespace VideoGameProject
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D pixel;
+        Texture2D background;
+        Texture2D sword;
 
         Texture2D PC_stagnant_FacingSouth;
         Texture2D PC_Moving1_FacingSouth;
@@ -52,7 +54,16 @@ namespace VideoGameProject
 
         private SpriteFont font;
         private int spritefont;
-        public List<Rectangle> PCHitbox = new List<Rectangle>();    
+        int gamestate = 0;
+        int cursorstate = 0;
+        public List<Rectangle> PCHitbox = new List<Rectangle>();
+        Rectangle swordcursor = new Rectangle(515, 250, 50, 50);
+        Rectangle backgroundRect = new Rectangle(0, 0, 1366, 768);
+        Rectangle backgroundRectSub = new Rectangle(162, 150, 998, 455);
+        Rectangle backgroundRectLeftWall = new Rectangle(160, 150, 10, 455);
+        Rectangle backgroundRectRightWall = new Rectangle(1150, 150, 10, 455);
+        Rectangle backgroundRectCeiling = new Rectangle(160, 150, 1000, 10);
+        Rectangle backgroundRectFloor = new Rectangle(160, 600, 1000, 10);
 
 
         Rectangle PC = new Rectangle(400, 250, 25, 25);
@@ -95,6 +106,8 @@ namespace VideoGameProject
             spriteBatch = new SpriteBatch(GraphicsDevice);
             pixel = Content.Load <Texture2D> ("pixel");
             font = Content.Load<SpriteFont>("spritefont");
+            background = Content.Load<Texture2D>("background");
+            sword = Content.Load<Texture2D>("sword");
             PC_stagnant_FacingSouth = Content.Load<Texture2D>("PC_stagnant_FacingSouth");
             PC_Moving1_FacingSouth = Content.Load<Texture2D>("PC_Moving1_FacingSouth");
             PC_Moving2_FacingSouth = Content.Load<Texture2D>("PC_Moving2_FacingSouth");
@@ -153,62 +166,92 @@ namespace VideoGameProject
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            PC.X = (int)PCpos.X + 20;
-            PC.Y = (int)PCpos.Y + 20;
-
-
-            if (kstate.IsKeyDown(Keys.Down) && kstate.IsKeyUp(Keys.Up))
+            if (gamestate == 0)
             {
-                PCfacing = "South";
-                PCpos.Y = PCpos.Y + 5;
-                PCmovementstate++;
-                if (PCmovementstate > 40)
-                    PCmovementstate = 1;
-            }
-            if (kstate.IsKeyUp(Keys.Down) && kstate.IsKeyUp(Keys.Up) && kstate.IsKeyUp(Keys.Left) &&  kstate.IsKeyUp(Keys.Right))
-            {
-                PCmovementstate = 0;
-            }
-            
-            if (kstate.IsKeyDown(Keys.Up) && kstate.IsKeyUp(Keys.Down))
-            {
-                PCfacing = "North";
-                PCpos.Y = PCpos.Y - 5;
-                PCmovementstate++;
-                if (PCmovementstate > 40)
-                    PCmovementstate = 1;
-            }
-            if (kstate.IsKeyDown(Keys.Right) && kstate.IsKeyUp(Keys.Left))
-            {
-                PCfacing = "East";
-                PCpos.X = PCpos.X + 5;
-                PCmovementstate++;
-                if (PCmovementstate > 40)
-                    PCmovementstate = 1;
-            }
-            if (kstate.IsKeyDown(Keys.Left) && kstate.IsKeyUp(Keys.Right))
-            {
-                PCfacing = "West";
-                PCpos.X = PCpos.X - 5;
-                PCmovementstate++;
-                if (PCmovementstate > 40)
-                    PCmovementstate = 1;
+                if (kstate.IsKeyDown(Keys.Down) && cursorstate < 2)
+                {
+                    cursorstate++;
+                }
+                if (kstate.IsKeyDown(Keys.Up) && cursorstate > 0)
+                {
+                    cursorstate--;
+                }
+                if (cursorstate == 0)
+                {
+                    swordcursor.Y = 250;
+                }
+                if (cursorstate == 1)
+                {
+                    swordcursor.Y = 350;
+                }
+                if (cursorstate >= 2)
+                {
+                    swordcursor.Y = 450;
+                }
             }
 
-            /*
-            if (kstate.IsKeyDown(Keys.Left) && kstate.IsKeyDown(Keys.Right))
-                PCmovementstate = 0;
-            if (kstate.IsKeyDown(Keys.Up) && kstate.IsKeyDown(Keys.Down))
-                PCmovementstate = 0;
-            if (kstate.IsKeyDown(Keys.Up) && kstate.IsKeyDown(Keys.Left))
-                PCmovementstate--;
-            if (kstate.IsKeyDown(Keys.Up) && kstate.IsKeyDown(Keys.Right))
-                PCmovementstate--;
-            if (kstate.IsKeyDown(Keys.Down) && kstate.IsKeyDown(Keys.Left))
-                PCmovementstate--;
-            if (kstate.IsKeyDown(Keys.Down) && kstate.IsKeyDown(Keys.Left))
-                PCmovementstate--;
-            */
+                
+
+            if (gamestate == 1)
+            {
+                PC.X = (int)PCpos.X + 20;
+                PC.Y = (int)PCpos.Y + 20;
+
+
+                if (kstate.IsKeyDown(Keys.Down) && kstate.IsKeyUp(Keys.Up))
+                {
+                    PCfacing = "South";
+                    PCpos.Y = PCpos.Y + 5;
+                    PCmovementstate++;
+                    if (PCmovementstate > 40)
+                        PCmovementstate = 1;
+                }
+                if (kstate.IsKeyUp(Keys.Down) && kstate.IsKeyUp(Keys.Up) && kstate.IsKeyUp(Keys.Left) && kstate.IsKeyUp(Keys.Right))
+                {
+                    PCmovementstate = 0;
+                }
+
+                if (kstate.IsKeyDown(Keys.Up) && kstate.IsKeyUp(Keys.Down))
+                {
+                    PCfacing = "North";
+                    PCpos.Y = PCpos.Y - 5;
+                    PCmovementstate++;
+                    if (PCmovementstate > 40)
+                        PCmovementstate = 1;
+                }
+                if (kstate.IsKeyDown(Keys.Right) && kstate.IsKeyUp(Keys.Left))
+                {
+                    PCfacing = "East";
+                    PCpos.X = PCpos.X + 5;
+                    PCmovementstate++;
+                    if (PCmovementstate > 40)
+                        PCmovementstate = 1;
+                }
+                if (kstate.IsKeyDown(Keys.Left) && kstate.IsKeyUp(Keys.Right))
+                {
+                    PCfacing = "West";
+                    PCpos.X = PCpos.X - 5;
+                    PCmovementstate++;
+                    if (PCmovementstate > 40)
+                        PCmovementstate = 1;
+                }
+
+                /*
+                if (kstate.IsKeyDown(Keys.Left) && kstate.IsKeyDown(Keys.Right))
+                    PCmovementstate = 0;
+                if (kstate.IsKeyDown(Keys.Up) && kstate.IsKeyDown(Keys.Down))
+                    PCmovementstate = 0;
+                if (kstate.IsKeyDown(Keys.Up) && kstate.IsKeyDown(Keys.Left))
+                    PCmovementstate--;
+                if (kstate.IsKeyDown(Keys.Up) && kstate.IsKeyDown(Keys.Right))
+                    PCmovementstate--;
+                if (kstate.IsKeyDown(Keys.Down) && kstate.IsKeyDown(Keys.Left))
+                    PCmovementstate--;
+                if (kstate.IsKeyDown(Keys.Down) && kstate.IsKeyDown(Keys.Left))
+                    PCmovementstate--;
+                */
+
+            }
 
 
             // TODO: Add your update logic here
@@ -224,68 +267,86 @@ namespace VideoGameProject
         {
             GraphicsDevice.Clear(Color.GreenYellow);
             spriteBatch.Begin();
-            spriteBatch.Draw(pixel, PC, Color.White);
-            if(PCfacing == "South" && PCmovementstate == 0)
-                spriteBatch.Draw(PC_stagnant_FacingSouth, PCpos, Color.White);
-            if (PCfacing == "South" && PCmovementstate > 0 && PCmovementstate < 5)
-                spriteBatch.Draw(PC_StartMoving1_FacingSouth, PCpos, Color.White);
-            if (PCfacing == "South" && PCmovementstate >= 5 && PCmovementstate < 14)
-                spriteBatch.Draw(PC_Moving1_FacingSouth, PCpos, Color.White);
-            if (PCfacing == "South" && PCmovementstate >= 14 && PCmovementstate < 20)             
-                spriteBatch.Draw(PC_StopMoving1_FacingSouth, PCpos, Color.White);
-            if (PCfacing == "South" && PCmovementstate >= 20 && PCmovementstate < 23)
-                spriteBatch.Draw(PC_StartMoving2_FacingSouth, PCpos, Color.White);
-            if (PCfacing == "South" && PCmovementstate >= 23 && PCmovementstate < 34)
-                spriteBatch.Draw(PC_Moving2_FacingSouth, PCpos, Color.White);
-            if (PCfacing == "South" && PCmovementstate >= 34 && PCmovementstate <= 40)
-                spriteBatch.Draw(PC_StopMoving2_FacingSouth, PCpos, Color.White);
 
-            if (PCfacing == "North" && PCmovementstate == 0)
-                spriteBatch.Draw(PC_stagnant_FacingNorth, PCpos, Color.White);
-            if (PCfacing == "North" && PCmovementstate > 0 && PCmovementstate < 5)
-                spriteBatch.Draw(PC_StartMoving1_FacingNorth, PCpos, Color.White);
-            if (PCfacing == "North" && PCmovementstate >= 5 && PCmovementstate < 14)
-                spriteBatch.Draw(PC_Moving1_FacingNorth, PCpos, Color.White);
-            if (PCfacing == "North" && PCmovementstate >= 14 && PCmovementstate < 20)
-                spriteBatch.Draw(PC_StopMoving1_FacingNorth, PCpos, Color.White);
-            if (PCfacing == "North" && PCmovementstate >= 20 && PCmovementstate < 23)
-                spriteBatch.Draw(PC_StartMoving2_FacingNorth, PCpos, Color.White);
-            if (PCfacing == "North" && PCmovementstate >= 23 && PCmovementstate < 34)
-                spriteBatch.Draw(PC_Moving2_FacingNorth, PCpos, Color.White);
-            if (PCfacing == "North" && PCmovementstate >= 34 && PCmovementstate <= 40)
-                spriteBatch.Draw(PC_StopMoving2_FacingNorth, PCpos, Color.White);
+            if (gamestate == 0)
+            {
+                spriteBatch.Draw(background, backgroundRect, Color.White);
+                spriteBatch.Draw(pixel, backgroundRectSub, Color.Black);
+                spriteBatch.Draw(pixel, backgroundRectLeftWall, Color.Red);
+                spriteBatch.Draw(pixel, backgroundRectRightWall, Color.Red);
+                spriteBatch.Draw(pixel, backgroundRectCeiling, Color.Red);
+                spriteBatch.Draw(pixel, backgroundRectFloor, Color.Red);
+                spriteBatch.DrawString(font, " ", new Vector2(550, 160), Color.White);
+                spriteBatch.DrawString(font, "Start", new Vector2(599, 250), Color.White);
+                spriteBatch.DrawString(font, "Controls", new Vector2(599, 350), Color.White);
+                spriteBatch.DrawString(font, "Exit", new Vector2(599, 450), Color.White);
+                spriteBatch.Draw(sword, swordcursor, Color.White);
+            }
 
-            if (PCfacing == "East" && PCmovementstate == 0)
-                spriteBatch.Draw(PC_stagnant_FacingEast, PCpos, Color.White);
-            if (PCfacing == "East" && PCmovementstate > 0 && PCmovementstate < 5)
-                spriteBatch.Draw(PC_StartMoving1_FacingEast, PCpos, Color.White);
-            if (PCfacing == "East" && PCmovementstate >= 5 && PCmovementstate < 14)
-                spriteBatch.Draw(PC_Moving1_FacingEast, PCpos, Color.White);
-            if (PCfacing == "East" && PCmovementstate >= 14 && PCmovementstate < 20)
-                spriteBatch.Draw(PC_StopMoving1_FacingEast, PCpos, Color.White);
-            if (PCfacing == "East" && PCmovementstate >= 20 && PCmovementstate < 23)
-                spriteBatch.Draw(PC_StartMoving2_FacingEast, PCpos, Color.White);
-            if (PCfacing == "East" && PCmovementstate >= 23 && PCmovementstate < 34)
-                spriteBatch.Draw(PC_Moving2_FacingEast, PCpos, Color.White);
-            if (PCfacing == "East" && PCmovementstate >= 34 && PCmovementstate <= 40)
-                spriteBatch.Draw(PC_StopMoving2_FacingEast, PCpos, Color.White);
+            if (gamestate == 1) //gamestate = 1 innebär att spelet är igång, då ska alltså menyn försvinna och karaktärer m.m ska kunna bli synliga. 
+            {
+                spriteBatch.Draw(pixel, PC, Color.White);
+                if (PCfacing == "South" && PCmovementstate == 0)
+                    spriteBatch.Draw(PC_stagnant_FacingSouth, PCpos, Color.White);
+                if (PCfacing == "South" && PCmovementstate > 0 && PCmovementstate < 5)
+                    spriteBatch.Draw(PC_StartMoving1_FacingSouth, PCpos, Color.White);
+                if (PCfacing == "South" && PCmovementstate >= 5 && PCmovementstate < 14)
+                    spriteBatch.Draw(PC_Moving1_FacingSouth, PCpos, Color.White);
+                if (PCfacing == "South" && PCmovementstate >= 14 && PCmovementstate < 20)
+                    spriteBatch.Draw(PC_StopMoving1_FacingSouth, PCpos, Color.White);
+                if (PCfacing == "South" && PCmovementstate >= 20 && PCmovementstate < 23)
+                    spriteBatch.Draw(PC_StartMoving2_FacingSouth, PCpos, Color.White);
+                if (PCfacing == "South" && PCmovementstate >= 23 && PCmovementstate < 34)
+                    spriteBatch.Draw(PC_Moving2_FacingSouth, PCpos, Color.White);
+                if (PCfacing == "South" && PCmovementstate >= 34 && PCmovementstate <= 40)
+                    spriteBatch.Draw(PC_StopMoving2_FacingSouth, PCpos, Color.White);
 
-            if (PCfacing == "West" && PCmovementstate == 0)
-                spriteBatch.Draw(PC_stagnant_FacingWest, PCpos, Color.White);
-            if (PCfacing == "West" && PCmovementstate > 0 && PCmovementstate < 5)
-                spriteBatch.Draw(PC_StartMoving1_FacingWest, PCpos, Color.White);
-            if (PCfacing == "West" && PCmovementstate >= 5 && PCmovementstate < 14)
-                spriteBatch.Draw(PC_Moving1_FacingWest, PCpos, Color.White);
-            if (PCfacing == "West" && PCmovementstate >= 14 && PCmovementstate < 20)
-                spriteBatch.Draw(PC_StopMoving1_FacingWest, PCpos, Color.White);
-            if (PCfacing == "West" && PCmovementstate >= 20 && PCmovementstate < 23)
-                spriteBatch.Draw(PC_StartMoving2_FacingWest, PCpos, Color.White);
-            if (PCfacing == "West" && PCmovementstate >= 23 && PCmovementstate < 34)
-                spriteBatch.Draw(PC_Moving2_FacingWest, PCpos, Color.White);
-            if (PCfacing == "West" && PCmovementstate >= 34 && PCmovementstate <= 40)
-                spriteBatch.Draw(PC_StopMoving2_FacingWest, PCpos, Color.White);
+                if (PCfacing == "North" && PCmovementstate == 0)
+                    spriteBatch.Draw(PC_stagnant_FacingNorth, PCpos, Color.White);
+                if (PCfacing == "North" && PCmovementstate > 0 && PCmovementstate < 5)
+                    spriteBatch.Draw(PC_StartMoving1_FacingNorth, PCpos, Color.White);
+                if (PCfacing == "North" && PCmovementstate >= 5 && PCmovementstate < 14)
+                    spriteBatch.Draw(PC_Moving1_FacingNorth, PCpos, Color.White);
+                if (PCfacing == "North" && PCmovementstate >= 14 && PCmovementstate < 20)
+                    spriteBatch.Draw(PC_StopMoving1_FacingNorth, PCpos, Color.White);
+                if (PCfacing == "North" && PCmovementstate >= 20 && PCmovementstate < 23)
+                    spriteBatch.Draw(PC_StartMoving2_FacingNorth, PCpos, Color.White);
+                if (PCfacing == "North" && PCmovementstate >= 23 && PCmovementstate < 34)
+                    spriteBatch.Draw(PC_Moving2_FacingNorth, PCpos, Color.White);
+                if (PCfacing == "North" && PCmovementstate >= 34 && PCmovementstate <= 40)
+                    spriteBatch.Draw(PC_StopMoving2_FacingNorth, PCpos, Color.White);
 
-            spriteBatch.DrawString(font, "spritefont", new Vector2(500, 400), Color.White);
+                if (PCfacing == "East" && PCmovementstate == 0)
+                    spriteBatch.Draw(PC_stagnant_FacingEast, PCpos, Color.White);
+                if (PCfacing == "East" && PCmovementstate > 0 && PCmovementstate < 5)
+                    spriteBatch.Draw(PC_StartMoving1_FacingEast, PCpos, Color.White);
+                if (PCfacing == "East" && PCmovementstate >= 5 && PCmovementstate < 14)
+                    spriteBatch.Draw(PC_Moving1_FacingEast, PCpos, Color.White);
+                if (PCfacing == "East" && PCmovementstate >= 14 && PCmovementstate < 20)
+                    spriteBatch.Draw(PC_StopMoving1_FacingEast, PCpos, Color.White);
+                if (PCfacing == "East" && PCmovementstate >= 20 && PCmovementstate < 23)
+                    spriteBatch.Draw(PC_StartMoving2_FacingEast, PCpos, Color.White);
+                if (PCfacing == "East" && PCmovementstate >= 23 && PCmovementstate < 34)
+                    spriteBatch.Draw(PC_Moving2_FacingEast, PCpos, Color.White);
+                if (PCfacing == "East" && PCmovementstate >= 34 && PCmovementstate <= 40)
+                    spriteBatch.Draw(PC_StopMoving2_FacingEast, PCpos, Color.White);
+
+                if (PCfacing == "West" && PCmovementstate == 0)
+                    spriteBatch.Draw(PC_stagnant_FacingWest, PCpos, Color.White);
+                if (PCfacing == "West" && PCmovementstate > 0 && PCmovementstate < 5)
+                    spriteBatch.Draw(PC_StartMoving1_FacingWest, PCpos, Color.White);
+                if (PCfacing == "West" && PCmovementstate >= 5 && PCmovementstate < 14)
+                    spriteBatch.Draw(PC_Moving1_FacingWest, PCpos, Color.White);
+                if (PCfacing == "West" && PCmovementstate >= 14 && PCmovementstate < 20)
+                    spriteBatch.Draw(PC_StopMoving1_FacingWest, PCpos, Color.White);
+                if (PCfacing == "West" && PCmovementstate >= 20 && PCmovementstate < 23)
+                    spriteBatch.Draw(PC_StartMoving2_FacingWest, PCpos, Color.White);
+                if (PCfacing == "West" && PCmovementstate >= 23 && PCmovementstate < 34)
+                    spriteBatch.Draw(PC_Moving2_FacingWest, PCpos, Color.White);
+                if (PCfacing == "West" && PCmovementstate >= 34 && PCmovementstate <= 40)
+                    spriteBatch.Draw(PC_StopMoving2_FacingWest, PCpos, Color.White);
+            }
+
 
 
             spriteBatch.End();
